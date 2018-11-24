17dda35571c65794f0b7343104dd123b13207662
Ashish Ranjan <infinitydon9@gmail.com>
2018-11-24 16:57:17 +0530

    Added components for basic navigation and project categories.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CategoryEditComponent } from './categories/category-edit/category-edit.component';
+import { CategoryDetailComponent } from './categories/category-detail/category-detail.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    CategoryEditComponent,
+    CategoryDetailComponent,
+    NavigationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
