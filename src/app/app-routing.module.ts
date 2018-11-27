af9db351d7746c464f5ee8fafc21f6dd968e4f77
Ashish Ranjan <infinitydon9@gmail.com>
2018-11-28 01:11:42 +0530

    Added filter pipe to implement search functionality on categories. Updated the routes to load categories details.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
+import { CategoryDetailComponent } from './categories/category-detail/category-detail.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from '@angular/core';
 
 const appRoutes: Routes = [
-    { path: 'categories', component: CategoriesComponent}
+    { path: 'categories', component: CategoriesComponent, children: [
+            { path: '', redirectTo: '/categories/1', pathMatch: 'full'},
+            { path: ':id', component: CategoryDetailComponent}
+        ]
+    }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { AppRoutingModule } from './app-routing.module';
 import { NavigationService } from './navigation/navigation.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { CategoryEditComponent } from './categories/category-edit/category-edit.
 import { CategoryDetailComponent } from './categories/category-detail/category-detail.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from './header/header.component';
+import { FilterPipe } from './shared/filter.pipe';
 
 @NgModule({
   declarations: [
@@ -17,10 +19,12 @@ import { HeaderComponent } from './header/header.component';
     CategoryEditComponent,
     CategoryDetailComponent,
     NavigationComponent,
-    HeaderComponent
+    HeaderComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [NavigationService],
