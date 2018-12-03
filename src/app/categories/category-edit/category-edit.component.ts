7be42e084e2a8fda353f9b10563e0d847d1e820b
Ashish Ranjan <infinitydon9@gmail.com>
2018-12-04 00:12:24 +0530

    Created form to add new project category using template driven approach. Implemented basic validation.

--- src/app/categories/category-edit/category-edit.component.ts
+++ src/app/categories/category-edit/category-edit.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-category-edit',
-  templateUrl: './category-edit.component.html',
-  styleUrls: ['./category-edit.component.css']
+    selector: 'app-category-edit',
+    templateUrl: './category-edit.component.html',
+    styleUrls: ['./category-edit.component.css']
 })
 export class CategoryEditComponent implements OnInit {
+    // Default Parent Category is Top.
+    defaultParent = "1";
+    constructor() { }
+    
+    ngOnInit() {
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    onSubmit(form: NgForm) {
+        console.log(form);
+    }
 }
